2c19002f71b36b764d8a363be2e03cd564bba8c0
Esten <esten.woien@gmail.com>
2021-10-05 10:26:14 +0200

    Changes to samplepaths

--- README.md
+++ README.md
@@ -27,4 +27,5 @@ plt.show()
 ![Ch](chess.svg)
 
 
+
 The current version supports analysis of open curves with fixed rotation, including registration of curves and computation of shape space geodesics and geodesic distances. It does not support analysis of closed curves, or curves modulo rotation.
--- srvpy/open/hjb.py
+++ srvpy/open/hjb.py
@@ -166,11 +166,10 @@ class HJBSolver:
             not specified, phi1 is set to (1,1).
         """
 
+        # Initialize the sequence
         n = self.shape[0]-1
         m = self.shape[1]-1
         psi = np.zeros(n+m+1)
-        phi = np.zeros((n+m+1,2))
-        phi[-1,:] = (n,m)
         eps = np.finfo(float).eps
 
         for k in reversed(range(1,n+m+1)):
@@ -183,10 +182,15 @@ class HJBSolver:
                 psi[k-1] = k-1
                 continue
 
+            # Transform psi to phi
             phi1 = 0.5*(k+psi[k])
             phi2 = 0.5*(k-psi[k])
+
+            # Find the grid cell
             i = int(floor(phi1))
             j = int(floor(phi2))
+
+            # Interpolate the optimal direction alpha
             if phi1 == i:
                 dpsi = np.diff(self.scheme.alpha(self.value,self,i,j))
             else:
@@ -195,6 +199,7 @@ class HJBSolver:
                 h1 = phi1-i
                 dpsi = h1*dpsir + (1-h1)*dpsil
 
+            # Compute the next step
             psi[k-1] = np.clip(psi[k]+dpsi,-(k-1),k-1)
 
         k = np.arange(n+m+1)
@@ -208,6 +213,7 @@ class HJBSolver:
         self.solve_hjb()
         phi = self.backtrack_hjb()
         self.resample(phi[:,0],phi[:,1])
+
 
     def f(self, i, j):
         return np.maximum(self.ip(self.q1sdt[i-1],self.q2sdt[j-1]),0)
@@ -228,14 +234,18 @@ class HJBSolver:
 
 
 
-class HJBSolverQ(HJBSolver):
+
+class HJBSolverSRS(HJBSolver):
     """
-    Class for solving the HJB equation when point evaluations of the SRVTs of
-    the curves are available.
+    Class for solving the HJB equation
+
+    max {-Du(x)(a) + <Q1(x1)sqrt(a1), Q2(x2)sqrt(a1)>} = 0
+
+    Where Q1 and Q2 are square root speed (SRS) curves.
     """
 
 
-    def __init__(self,q1,q2,x1,x2=None,scheme=VInf,unit_sphere=False,normalize=False):
+    def __init__(self,q1sdt,q2sdt,x1,x2=None,scheme=VInf,unit_sphere=False,normalize=False):
         """
         Initializes a solver for the HJB equation based on SRVTs of the curves.
 
@@ -264,15 +274,24 @@ class HJBSolverQ(HJBSolver):
         elif isinstance(x2, numbers.Integral):
             x2 = np.linspace(0,1,x2+1)
 
-        self.q1 = q1
-        self.q2 = q2
         self.scheme = scheme
         self.unit_sphere = unit_sphere
         self.normalize = normalize
 
-        # Find the dimension of the curves
-        q101 = q1(np.array([0,1]))
-        q201 = q2(np.array([0,1]))
+        # Ensure that the functions are callable with ndarrays as input and
+        # find the dimension of the curves
+        try:
+            q101 = q1(np.array([0,1]))
+            self.q1 = q1
+        except:
+            self.q1 = lambda t: np.array([q1(t) for t in t])
+            q101 = self.q1(np.array([0,1]))
+        try:
+            q201 = q2(np.array([0,1]))
+            self.q2 = q2
+        except:
+            self.q2 = lambda t: np.array([q2(t) for t in t])
+            q201 = self.q2(np.array([0,1]))
 
         # Specify the according inner product
         if q101.ndim == 1 and q201.ndim == 1:
@@ -287,6 +306,7 @@ class HJBSolverQ(HJBSolver):
             raise ValueError("The SRVTs must provide curves of same dimension")
 
         self.resample(x1,x2)
+        self.rotation = 1
 
 
     def resample(self,x1,x2):
@@ -400,6 +420,187 @@ class HJBSolverQ(HJBSolver):
 
 
 
+
+
+class HJBSolverQ(HJBSolver):
+    """
+    Class for solving the HJB equation when point evaluations of the SRVTs of
+    the curves are available.
+    """
+
+
+    def __init__(self,q1,q2,x1,x2=None,scheme=VInf,unit_sphere=False,normalize=False):
+        """
+        Initializes a solver for the HJB equation based on SRVTs of the curves.
+
+        Parameters
+        scheme : Scheme
+            The scheme used by the solver
+        q1, q2 : callable
+            Functions representing the SRVTs of the curves. The functions
+            accepts an ndarray (n,) and return either a real or complex
+            ndarray (n,) or a real or complex ndarray (n,d).
+        x1, x2 : ndarray (n,) or int
+            Grids used for solving the HJB equation. If x1 is int, then the
+            a uniform grid with x1+1 points will be used. Similar for x2.
+        unit_sphere : bool
+            If True, we use the unit sphere topology to compute geodesics.
+            If False, we use the L2 topology.
+        normalize : bool
+            If True, the curves are normalized to be approximately unit length.
+        """
+
+        # Construct the grids
+        if isinstance(x1, numbers.Integral):
+            x1 = np.linspace(0,1,x1+1)
+        if x2 is None:
+            x2 = x1
+        elif isinstance(x2, numbers.Integral):
+            x2 = np.linspace(0,1,x2+1)
+
+        self.scheme = scheme
+        self.unit_sphere = unit_sphere
+        self.normalize = normalize
+
+        # Ensure that the functions are callable with ndarrays as input and
+        # find the dimension of the curves
+        try:
+            q101 = q1(np.array([0,1]))
+            self.q1 = q1
+        except:
+            self.q1 = lambda t: np.array([q1(t) for t in t])
+            q101 = self.q1(np.array([0,1]))
+        try:
+            q201 = q2(np.array([0,1]))
+            self.q2 = q2
+        except:
+            self.q2 = lambda t: np.array([q2(t) for t in t])
+            q201 = self.q2(np.array([0,1]))
+
+        # Specify the according inner product
+        if q101.ndim == 1 and q201.ndim == 1:
+            self.ndim = 1
+            self.ip = lambda q1,q2: (q1*q2.conj()).real
+            self.op = lambda q1,q2: q1*q2.conj()
+        elif c101.ndim > 1 and c201.ndim == c101.ndim:
+            self.ndim = c101.shape[1]
+            self.ip = lambda q1,q2: (q1*q2.conj()).real.sum(axis=-1)
+            self.op = lambda q1,q2: q1@q2.conj().transpose()
+        else:
+            raise ValueError("The SRVTs must provide curves of same dimension")
+
+        self.resample(x1,x2)
+        self.rotation = 1
+
+
+    def resample(self,x1,x2):
+        """
+        Sets the grid and samples the SRVTs.
+
+        Parameters
+        ----------
+        x1,x2 : ndarray
+            Ordered sequences representing the grid.
+        """
+
+        self.x1 = x1
+        self.x2 = x2
+        self.shape = (self.x1.shape[0],self.x2.shape[0])
+
+        # Compute sqrt(dx)
+        sdx1 = np.sqrt(np.diff(x1))
+        sdx2 = np.sqrt(np.diff(x1))
+        if self.ndim == 1:
+            sdx1 = sdx1[:,None]
+            sdx2 = sdx2[:,None]
+
+        # Compute q*sqrt(dx) and the lengths of the curves
+        self.q1sdt = self.q1(x1[1:])*sdx1
+        self.q2sdt = self.q2(x2[1:])*sdx2
+        self.l1 = np.linalg.norm(self.q1sdt)**2
+        self.l2 = np.linalg.norm(self.q2sdt)**2
+
+        # Normalize qsdt
+        if self.normalize:
+            self.q1sdt /= np.sqrt(self.l1)
+            self.q2sdt /= np.sqrt(self.l2)
+
+
+    def __getitem__(self, i):
+        """
+        Returns fij * sqrt(dxi * dyj).
+        """
+        return np.maximum(self.ip(self.q1sdt[i[0]-1],self.q2sdt[i[1]-1]),0)
+
+
+    def geodesic(self,tau,output='curve',length='invariant',shift=False):
+        """
+        Computes
+        """
+
+        # Specify the weighting function
+        if self.unit_sphere:
+            dist = self.distance()
+            if dist==0:
+                w = lambda t: t
+            w = lambda t: np.sin(dist*t)/np.sin(dist)
+        else:
+            w = lambda t: t
+
+        # Compute the srvts
+        if self.ndim == 1:
+            tau = tau[:,None]
+            q = self.q1sdt[None,:]*w(1-tau) + self.q2sdt[None,:]*w(tau)
+        else:
+            tau = tau[:,None,None]
+            q = self.q1sdt[None,:,:]*w(1-tau) + self.q2sdt[None,:,:]*w(tau)
+
+        # Scale by length
+        length = length.lower()
+        if (length)=='invariant':
+            l = 1
+        elif (length)=='linear':
+            l = (1-tau)*self.l1 + tau*self.l2
+        elif (length)=='logarithmic':
+            l = np.exp((1-tau)*np.log(self.l1) + tau*np.log(self.l2))
+        else:
+            raise ValueError("Unknown length type '" + length + "'")
+        q *= np.sqrt(l)
+
+        output = output.lower()
+        if (output)=='srvt':
+            return q
+        elif (output)=='curve':
+            if self.ndim == 1:
+                c = np.cumsum(q*np.sqrt(self.ip(q,q)),axis=1)
+                if shift is not False:
+                    width = c.real.ptp(axis=1)
+                    width += shift*width.mean()
+                    c += width.cumsum()[:,None]
+            else:
+                c = np.cumsum(q*np.sqrt(self.ip(q,q))[:,:,None],axis=1)
+            return c
+        else:
+            raise ValueError("Unknown output type '" + output + "'")
+
+    def inner_product(self):
+        return self.objective()
+
+
+    def distance(self):
+        """
+        Computes the pre-shape space distance between the curves.
+        """
+        if self.unit_sphere:
+            return np.arccos(min(1,self.inner_product()))
+        if self.normalize:
+            return np.sqrt(2-2*self.inner_product())
+        return np.sqrt(self.l1+self.l2-2*self.inner_product())
+
+
+
+
+
 class HJBSolverC(HJBSolverQ):
     """
     Class for solving the HJB equation when point evaluations of the curves are
@@ -435,15 +636,24 @@ class HJBSolverC(HJBSolverQ):
         elif isinstance(x2, numbers.Integral):
             x2 = np.linspace(0,1,x2+1)
 
-        self.c1 = c1
-        self.c2 = c2
         self.scheme = scheme
         self.unit_sphere = unit_sphere
         self.normalize = normalize
 
-        # Find the dimension of the curves
-        c101 = c1(np.array([0,1]))
-        c201 = c2(np.array([0,1]))
+        # Ensure that the functions are callable with ndarrays as input and
+        # find the dimension of the curves
+        try:
+            c101 = c1(np.array([0,1]))
+            self.c1 = c1
+        except:
+            self.c1 = lambda t: np.array([c1(t) for t in t])
+            c101 = self.c1(np.array([0,1]))
+        try:
+            c201 = c2(np.array([0,1]))
+            self.c2 = c2
+        except:
+            self.c2 = lambda t: np.array([c2(t) for t in t])
+            c201 = self.c2(np.array([0,1]))
 
         # Specify the according inner product
         if c101.ndim == 1 and c201.ndim == 1:
@@ -458,6 +668,7 @@ class HJBSolverC(HJBSolverQ):
             raise ValueError("The SRVTs must provide curves of same dimension")
 
         self.resample(x1,x2)
+        self.rotation = 1
 
 
     def resample(self,x1,x2):
